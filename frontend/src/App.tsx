9e6e616b4eb268f421232662ebda6c64ff522eb2
t-tsunemi <takatsune1003@gmail.com>
2026-05-04 01:25:10 +0900

    style: format by prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 function App() {
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold underline">
-        Hello!
-      </h1>
-      <button className="btn btn-primary mt-4">
-        daisyUI Button
-      </button>
+      <h1 className="text-3xl font-bold underline">Hello!</h1>
+      <button className="btn btn-primary mt-4">daisyUI Button</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
